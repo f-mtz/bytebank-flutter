eb05ed6ed64101185357471aac142ba60b9acde8
DESKTOP-HA4DR23\felip <mtz.dev.si@gmail.com>
2022-01-01 20:12:23 -0300

    Refatorando os componentes

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    body: Column(
-      children: [
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('saldo...'),
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('200.0'),
-            subtitle: Text('saldo...'),
-          ),
-        ),
-      ],
-    ),
+    body: ListaTransferencias(),
     appBar: AppBar(title: Text('Transferências'),),
     floatingActionButton: FloatingActionButton(
         onPressed: () {  },
@@ -28,3 +11,43 @@ void main() => runApp(MaterialApp(
   ),
 ));
 
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.0, 1001)),
+        ItemTransferencia(Transferencia(300.0, 1002)),
+        ItemTransferencia(Transferencia(600.0, 1003))
+      ],
+    );
+  }
+
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroDaConta.toString()),
+      ),
+    );
+  }
+  
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroDaConta;
+
+  Transferencia(this.valor, this.numeroDaConta);
+}
