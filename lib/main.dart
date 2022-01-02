4383a44dd174fe07690856ae8ffbdb299fff3cdd
DESKTOP-HA4DR23\felip <mtz.dev.si@gmail.com>
2022-01-01 21:06:07 -0300

    Criando tela de formulario

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,45 @@ class FormularioTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-
       appBar: AppBar(
         title: Text('Criando Transferência'),
       ),
-      body: Text('texto2'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+
+          RaisedButton(
+            onPressed: () {  },
+            child: Text('Confirmar'),
+          ),
+        ],
+      ),
     );
   }
 }
